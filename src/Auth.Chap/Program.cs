1254247724ebfea451485eafcb02c103461da68d
Vladlen <vladlenafonin.university@yandex.ru>
2022-02-26 12:48:52 +0300

    Folder structure redone

---
+++ src/Auth.Chap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auth.Chap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
